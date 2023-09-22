48a336ee9576e352c0aac6b45eade6c56d01c9ac
ottovenliu <otto.ven.liu@gmail.com>
2023-09-22 11:30:26 +0800

    feat: 安裝Tailwind_&_路由初始化_&_layout初始化

---
+++ src/app/feature/e-shopping/shopping-home/shopping-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './shopping-home.component.html',
+  styleUrls: ['./shopping-home.component.scss']
+})
+export class ShoppingHomeComponent {
+
+}
