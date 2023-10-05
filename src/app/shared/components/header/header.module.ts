77563c8dc6d1904db7276c747d844820860e2970
OttoLiu <otto.ven.liu@gmail.com>
2023-10-05 21:39:03 +0800

    feat: slideMenu - otto

--- src/app/shared/components/header/header.module.ts
+++ src/app/shared/components/header/header.module.ts
@@ -4,9 +4,20 @@ import { HeaderComponent } from './header.component';
 import { SwiperModule } from 'swiper/angular';
 import { RouterModule } from '@angular/router';
 import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { BlockUIModule } from 'primeng/blockui';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, SwiperModule, RouterModule, SidebarModule],
+  imports: [
+    CommonModule,
+    SwiperModule,
+    RouterModule,
+    SidebarModule,
+    ButtonModule,
+    BlockUIModule,
+    ProgressSpinnerModule,
+  ],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
